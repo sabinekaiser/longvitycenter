7d018274a533505aa82624f2555bd0de6a2a218d
bodokaiser <i@bodokaiser.io>
2013-08-17 11:53:45 +0200

    moved translations directly into templates

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,11 @@
-var util = require('util');
-var fs   = require('fs');
-
-var trans = {};
-
-trans.en = loadTrans('en');
-trans.de = loadTrans('de');
-
 module.exports = function(app) {
 
     app.all('/', function(req, res) {
         res.redirect('/de');
     });
 
-    app.all('/:lang', function(req, res) {
-        res.render('index', trans[req.params.lang]);
+    app.all('/:language', function(req, res) {
+        res.render('index', { language: req.params.language });
     });
 
 };
-
-function loadTrans(lang) {
-    var path = util.format(__dirname + '/../trans/%s.json', lang);
-
-    var buf = fs.readFileSync(path);
-    var str = buf.toString();
-
-    return JSON.parse(str);
-}
