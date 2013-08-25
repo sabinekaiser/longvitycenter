c8b9320b8c3b989689698ed094c51891c4fae61c
bodokaiser <i@bodokaiser.io>
2013-08-25 16:20:35 +0200

    updated config module

--- config/index.js
+++ config/index.js
@@ -1,1 +1,16 @@
-module.exports = require('./environment');
+var config = require('./environment');
+
+var environment = process.env.NODE_ENV || 'development';
+
+if (environment == 'development') 
+    merge(config, require('./development'));
+
+if (environment == 'production') 
+    merge(config, require('./production'));
+
+module.exports = config;
+
+function merge(source, object) {
+    for (var key in object)
+        source[key] = object[key];
+}
